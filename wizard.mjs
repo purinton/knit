6db93ef7a7fb57759084ecfd12274e2cb759d28f
Russell Purinton <russell.purinton@gmail.com>
2025-06-24 01:01:09 +0000

    Version 1.0.4 - 2025-06-24

--- wizard.mjs
+++ wizard.mjs
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 import 'dotenv/config';
 import { log, registerHandlers, registerSignals } from '@purinton/common';
-import { runWizard } from './src/wizard.mjs';
 
 registerHandlers({ log });
 registerSignals({ log });
 
 log.info('Knit Wizard starting...');
 
-runWizard();
+import { runWizard } from './src/wizard.mjs';
